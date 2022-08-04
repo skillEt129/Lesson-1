595f5f899ac1888c6c7ff6ecdf41f3994edf9089
Roman <roma-popov-98@inbox.ru>
2022-08-04 21:06:49 +0400

    if_else

---
+++ ex05_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "роман") // ToLower - позволяет ысе символы строки перевести в нижний регистр.
+{
+    Console.WriteLine("Ура, это же Роман!!!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
