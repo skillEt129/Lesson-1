576b14101d33b746157679f258b370d3eefa0261
Roman <roma-popov-98@inbox.ru>
2022-08-04 21:00:15 +0400

    Base

---
+++ ex06_Base/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 1;
+int b = 2;
+int c = 4;
+int d = 6;
+int e = 8;
+int f = 5;
+int g = 3;
+int max = a;
+if (b>max) max= b;
+if (c>max) max= c;
+if (d>max) max= d;
+if (e>max) max= e;
+if (f>max) max= f;
+if (g>max) max= g;
+Console.Write("max = ");
+Console.WriteLine(max);
